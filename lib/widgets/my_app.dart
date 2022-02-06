6e9ae1c3823c377672a4f61fe92039a4c35d3320
James Leahy <defuncart@gmail.com>
2022-02-06 11:49:34 +0100

    Init settings database in MyApp.

--- lib/widgets/my_app.dart
+++ lib/widgets/my_app.dart
@@ -1,20 +1,72 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:photo_album/design_system/themes/adaptive_platform.dart';
 import 'package:photo_album/design_system/widgets/adaptive_app.dart';
 import 'package:photo_album/design_system/widgets/adaptive_radio_button_group.dart';
 import 'package:photo_album/generated/l10n.dart';
+import 'package:photo_album/state/state.dart';
 
-class MyApp extends StatefulWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends ConsumerState<MyApp> {
+  var _isInitializing = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initApp();
+  }
+
+  Future<void> _initApp() async {
+    await ref.read(hiveManagerProvider).initialize();
+    await ref.read(settingsDatabaseProvider).initialize();
+
+    setState(() => _isInitializing = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isInitializing) {
+      return const LoadingWidget();
+    }
+
+    return const MyAppContent();
+  }
+}
+
+@visibleForTesting
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => const SizedBox.shrink();
+}
+
+@visibleForTesting
+class MyAppContent extends ConsumerStatefulWidget {
+  const MyAppContent({Key? key}) : super(key: key);
+
+  @override
+  __MyAppState createState() => __MyAppState();
+}
+
+class __MyAppState extends ConsumerState<MyAppContent> {
   var _platform = PlatformType.windows;
-  var _themeMode = ThemeMode.system;
+  late ThemeMode _themeMode;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _themeMode = ref.read(settingsDatabaseProvider).themeMode;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +95,10 @@ class _MyAppState extends State<MyApp> {
                           ))
                       .toList(),
                   selectedValue: _themeMode,
-                  onChanged: (value) => setState(() => _themeMode = value),
+                  onChanged: (value) {
+                    ref.read(settingsDatabaseProvider).themeMode = value;
+                    setState(() => _themeMode = value);
+                  },
                 ),
               ],
             ),
