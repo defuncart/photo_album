a93c5566f1004bfc9af9af989a0714174fefbe89
James Leahy <defuncart@gmail.com>
2022-02-06 11:18:25 +0100

    Add more SettingsDatabase tests.

--- lib/services/settings_database/settings_database.dart
+++ lib/services/settings_database/settings_database.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart' show ThemeMode;
 import 'package:hive/hive.dart';
+import 'package:meta/meta.dart' show visibleForTesting;
 import 'package:photo_album/services/settings_database/i_settings_database.dart';
 
 class SettingsDatabase implements ISettingsDatabase {
@@ -28,7 +29,12 @@ class SettingsDatabase implements ISettingsDatabase {
   set language(String? value) => _box.put(_Keys.language, value);
 
   @override
-  Future<void> initialize() => _hive.openBox<dynamic>(_boxName);
+  Future<void> initialize() async {
+    if (!_hive.isAdapterRegistered(themeModeAdapterTypeId)) {
+      _hive.registerAdapter(ThemeModeAdapter());
+    }
+    _box = await _hive.openBox<dynamic>(_boxName);
+  }
 
   @override
   Future<void> reset() => _box.deleteAll(_box.keys);
@@ -44,4 +50,49 @@ class _Keys {
 class _Defaults {
   static const themeMode = ThemeMode.system;
   static const language = null;
+}
+
+@visibleForTesting
+const themeModeAdapterTypeId = 0;
+
+@visibleForTesting
+class ThemeModeAdapter extends TypeAdapter<ThemeMode> {
+  @override
+  final int typeId = themeModeAdapterTypeId;
+
+  @override
+  ThemeMode read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return ThemeMode.dark;
+      case 1:
+        return ThemeMode.light;
+      case 2:
+        return ThemeMode.system;
+      default:
+        return ThemeMode.system;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, ThemeMode obj) {
+    switch (obj) {
+      case ThemeMode.dark:
+        writer.writeByte(0);
+        break;
+      case ThemeMode.light:
+        writer.writeByte(1);
+        break;
+      case ThemeMode.system:
+        writer.writeByte(2);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || other is ThemeModeAdapter && runtimeType == other.runtimeType && typeId == other.typeId;
 }
--- test/services/settings_database/settings_database_test.dart
+++ test/services/settings_database/settings_database_test.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart' show ThemeMode;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hive/hive.dart';
 import 'package:mockito/mockito.dart';
@@ -21,10 +24,66 @@ void main() {
         when(mockHiveInterface.openBox('settings')).thenAnswer((_) => Future.value(MockBox()));
       });
 
+      test('expect that adapter is registered', () async {
+        when(mockHiveInterface.isAdapterRegistered(themeModeAdapterTypeId)).thenReturn(false);
+
+        await settingsDatabase.initialize();
+
+        verify(mockHiveInterface.registerAdapter(ThemeModeAdapter()));
+      });
+
       test('expect that box is opened', () async {
+        when(mockHiveInterface.isAdapterRegistered(themeModeAdapterTypeId)).thenReturn(true);
+
         await settingsDatabase.initialize();
 
         verify(mockHiveInterface.openBox('settings'));
+      });
+    });
+
+    group('Ensure hive works as expected', () {
+      const path = 'path';
+
+      setUp(() async {
+        await Directory(path).create(recursive: true);
+
+        Hive.init('path');
+
+        settingsDatabase = SettingsDatabase();
+        await settingsDatabase.initialize();
+      });
+
+      tearDown(() async {
+        // delete files
+        final dir = Directory(path);
+        await dir.delete(recursive: true);
+      });
+
+      test('themeMode', () async {
+        const defaultThemMode = ThemeMode.system;
+        expect(settingsDatabase.themeMode, defaultThemMode);
+
+        settingsDatabase.themeMode = ThemeMode.dark;
+
+        expect(settingsDatabase.themeMode, ThemeMode.dark);
+
+        await settingsDatabase.reset();
+
+        expect(settingsDatabase.themeMode, defaultThemMode);
+      });
+
+      test('language', () async {
+        const defaultValue = null;
+
+        expect(settingsDatabase.language, defaultValue);
+
+        settingsDatabase.language = 'de';
+
+        expect(settingsDatabase.language, 'de');
+
+        await settingsDatabase.reset();
+
+        expect(settingsDatabase.language, defaultValue);
       });
     });
   });
