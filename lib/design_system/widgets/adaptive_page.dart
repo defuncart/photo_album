7070e2f9504121df4c20b219d6e4cea6d37d0979
James Leahy <defuncart@gmail.com>
2022-02-23 21:23:56 +0100

    Add AdaptivePage

---
+++ lib/design_system/widgets/adaptive_page.dart
@@ -0,0 +1,44 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:macos_ui/macos_ui.dart';
+import 'package:photo_album/design_system/themes/adaptive_platform.dart';
+
+class AdaptivePage extends StatelessWidget {
+  const AdaptivePage({
+    required this.title,
+    required this.child,
+    Key? key,
+  }) : super(key: key);
+
+  final String title;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final adaptivePlatform = AdaptivePlatform.of(context);
+    if (adaptivePlatform.isMacOS) {
+      return MacosScaffold(
+        titleBar: TitleBar(
+          title: Text(title),
+        ),
+        children: [
+          ContentArea(
+            builder: (context, controller) => child,
+          ),
+        ],
+      );
+    } else if (adaptivePlatform.isLinux) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: child,
+      );
+    } else {
+      return ScaffoldPage(
+        header: Text(title),
+        content: child,
+      );
+    }
+  }
+}
