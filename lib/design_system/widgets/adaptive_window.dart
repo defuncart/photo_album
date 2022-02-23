aa7652a7a114c10329120b9761744aa48cd4d026
James Leahy <defuncart@gmail.com>
2022-02-23 21:40:27 +0100

    AdaptiveWindow add items, builders

--- lib/design_system/widgets/adaptive_window.dart
+++ lib/design_system/widgets/adaptive_window.dart
@@ -2,22 +2,34 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:macos_ui/macos_ui.dart';
+import 'package:photo_album/design_system/models/sidebar_item_model.dart';
 import 'package:photo_album/design_system/themes/adaptive_platform.dart';
 import 'package:photo_album/generated/l10n.dart';
 
 class AdaptiveWindow extends StatefulWidget {
   const AdaptiveWindow({
-    required this.child,
+    required this.sidebarItems,
+    required this.settingsItem,
+    required this.sidebarItemBuilder,
+    required this.settingsBuilder,
     Key? key,
   }) : super(key: key);
 
-  final Widget child;
+  final List<SidebarItemModel> sidebarItems;
+  final SidebarItemModel settingsItem;
+  final Widget Function(int) sidebarItemBuilder;
+  final WidgetBuilder settingsBuilder;
 
   @override
   State<AdaptiveWindow> createState() => _AdaptiveWindowState();
 }
 
 class _AdaptiveWindowState extends State<AdaptiveWindow> {
+  static const _transparent = Color(0x00000000);
+
+  var _currentIndex = 0;
+  var _isSettingsActive = false;
+
   @override
   Widget build(BuildContext context) {
     final adaptivePlatform = AdaptivePlatform.of(context);
@@ -26,57 +38,52 @@ class _AdaptiveWindowState extends State<AdaptiveWindow> {
         sidebar: Sidebar(
           builder: (context, controller) {
             return SidebarItems(
-              currentIndex: 0,
-              onChanged: print,
+              currentIndex: _currentIndex,
+              onChanged: (value) => setState(() {
+                _currentIndex = value;
+                _isSettingsActive = false;
+              }),
               scrollController: controller,
-              selectedColor: MacosTheme.of(context).dividerColor,
+              selectedColor: _isSettingsActive ? _transparent : MacosTheme.of(context).dividerColor,
               items: [
-                SidebarItem(
-                  leading: const MacosIcon(CupertinoIcons.heart),
-                  label: Text(
-                    'My Album',
-                    style: TextStyle(
-                      color: MacosTheme.of(context).typography.body.color,
+                for (final item in widget.sidebarItems)
+                  SidebarItem(
+                    leading: Icon(item.icon),
+                    label: Text(
+                      item.label,
+                      style: TextStyle(
+                        color: MacosTheme.of(context).typography.body.color,
+                      ),
                     ),
                   ),
-                ),
-                SidebarItem(
-                  leading: const MacosIcon(CupertinoIcons.compass),
-                  label: Text(
-                    'Flutter',
-                    style: TextStyle(
-                      color: MacosTheme.of(context).typography.body.color,
-                    ),
-                  ),
-                ),
               ],
             );
           },
           minWidth: 200,
-          bottom: MacosListTile(
-            leading: const MacosIcon(CupertinoIcons.settings),
-            title: Text(
-              'Settings',
-              style: TextStyle(
-                color: MacosTheme.of(context).typography.body.color,
+          bottom: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 12),
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+            decoration: BoxDecoration(
+              color: _isSettingsActive ? MacosTheme.of(context).dividerColor : _transparent,
+              borderRadius: const BorderRadius.all(Radius.circular(7)),
+            ),
+            child: MacosListTile(
+              leading: const MacosIcon(CupertinoIcons.settings),
+              title: Text(
+                'Settings',
+                style: TextStyle(
+                  color: MacosTheme.of(context).typography.body.color,
+                ),
               ),
+              onClick: () => setState(() => _isSettingsActive = true),
             ),
           ),
         ),
-        child: MacosScaffold(
-          titleBar: const TitleBar(
-            title: Text('Title Bar'),
-          ),
-          children: [
-            ContentArea(
-              builder: (context, controller) => widget.child,
-            ),
-          ],
-        ),
+        child: _isSettingsActive ? widget.settingsBuilder(context) : widget.sidebarItemBuilder(_currentIndex),
       );
     } else if (adaptivePlatform.isLinux) {
       return Scaffold(
-        body: widget.child,
+        body: _isSettingsActive ? widget.settingsBuilder(context) : widget.sidebarItemBuilder(_currentIndex),
       );
     } else {
       return NavigationView(
@@ -114,7 +121,7 @@ class _AdaptiveWindowState extends State<AdaptiveWindow> {
           ],
         ),
         content: ScaffoldPage(
-          content: widget.child,
+          content: _isSettingsActive ? widget.settingsBuilder(context) : widget.sidebarItemBuilder(_currentIndex),
         ),
       );
     }
--- lib/widgets/home_screen/home_screen.dart
+++ lib/widgets/home_screen/home_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:photo_album/design_system/models/sidebar_item_model.dart';
+import 'package:photo_album/design_system/widgets/adaptive_page.dart';
+import 'package:photo_album/design_system/widgets/adaptive_window.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox.shrink();
+    return AdaptiveWindow(
+      sidebarItems: const [
+        SidebarItemModel(
+          icon: Icons.abc,
+          label: 'abc',
+        ),
+      ],
+      settingsItem: const SidebarItemModel(
+        icon: Icons.abc,
+        label: 'abc',
+      ),
+      sidebarItemBuilder: (index) => const AdaptivePage(
+        title: 'Sidebar',
+        child: Text('info'),
+      ),
+      settingsBuilder: (context) => const AdaptivePage(
+        title: 'Settings',
+        child: Text('info'),
+      ),
+    );
   }
 }
