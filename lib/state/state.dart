da0a12cb82aae411fc19790675a595ef17cba0df
James Leahy <defuncart@gmail.com>
2022-02-24 17:53:46 +0100

    Add themeModeProvider, platformTypeProvider.

--- lib/state/state.dart
+++ lib/state/state.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:photo_album/design_system/themes/adaptive_platform.dart';
 import 'package:photo_album/services/hive/hive_manager.dart';
 import 'package:photo_album/services/settings_database/i_settings_database.dart';
 import 'package:photo_album/services/settings_database/settings_database.dart';
@@ -10,3 +11,7 @@ final settingsDatabaseProvider = Provider<ISettingsDatabase>(
 final hiveManagerProvider = Provider(
   (_) => HiveManager(),
 );
+
+final themeModeProvider = StateProvider((ref) => ref.watch(settingsDatabaseProvider).themeMode);
+
+final platformTypeProvider = StateProvider((_) => PlatformType.system);
