d0924a4db6f8400589c0783995ac4f0c7dc94444
James Leahy <defuncart@gmail.com>
2022-02-26 10:34:20 +0100

    PhotoAlbum reorderable POC

--- lib/widgets/home_screen/photo_album_page/photo_album_page.dart
+++ lib/widgets/home_screen/photo_album_page/photo_album_page.dart
@@ -1,11 +1,12 @@
-import 'dart:developer' show log;
-
 import 'package:flutter/material.dart';
 import 'package:photo_album/design_system/widgets/adaptive_page.dart';
+import 'package:photo_album/generated/l10n.dart';
 import 'package:photo_album/services/albums_database/models/photo_album.dart';
 import 'package:photo_album/widgets/home_screen/photo_album_page/image_drop_target.dart';
+import 'package:photo_album/widgets/home_screen/photo_album_page/photo_widget.dart';
+import 'package:reorderables/reorderables.dart';
 
-class PhotoAlbumPage extends StatelessWidget {
+class PhotoAlbumPage extends StatefulWidget {
   const PhotoAlbumPage({
     required this.photoAlbum,
     Key? key,
@@ -14,13 +15,72 @@ class PhotoAlbumPage extends StatelessWidget {
   final PhotoAlbum photoAlbum;
 
   @override
+  State<PhotoAlbumPage> createState() => _PhotoAlbumPageState();
+}
+
+class _PhotoAlbumPageState extends State<PhotoAlbumPage> {
+  final _paths = <String>[];
+  int? _draggedIndex;
+
+  @override
   Widget build(BuildContext context) {
     return AdaptivePage(
-      title: photoAlbum.name,
+      title: widget.photoAlbum.name,
       child: ImageDropTarget(
-        onImagesDragged: (paths) => log(paths.toString()),
-        child: const SizedBox.expand(),
+        onImagesDragged: (paths) {
+          if (paths.isEmpty) {
+            // show popup
+          }
+
+          setState(() {
+            _paths
+              ..addAll(paths)
+              ..distinct();
+          });
+        },
+        child: SizedBox.expand(
+          child: _paths.isEmpty
+              ? Center(
+                  child: Text(AppLocalizations.of(context).photoAlbumPageEmptyStateLabel),
+                )
+              : ReorderableWrap(
+                  spacing: 8,
+                  runSpacing: 8,
+                  padding: const EdgeInsets.all(8),
+                  children: [
+                    for (var i = 0; i < _paths.length; i++)
+                      Opacity(
+                        opacity: _draggedIndex != null && _draggedIndex != i ? 0.4 : 1.0,
+                        child: PhotoWidget(
+                          path: _paths[i],
+                          width: 200,
+                          height: 125,
+                        ),
+                      ),
+                  ],
+                  onReorder: _onReorder,
+                  buildDraggableFeedback: (_, __, widget) => MouseRegion(
+                    cursor: SystemMouseCursors.help,
+                    child: widget,
+                  ),
+                  onNoReorder: (_) => setState(() => _draggedIndex = null),
+                  onReorderStarted: (index) => setState(() => _draggedIndex = index),
+                ),
+        ),
       ),
     );
   }
+
+  void _onReorder(int oldIndex, int newIndex) {
+    if (oldIndex != newIndex) {
+      // setState(() {
+      _paths.insert(newIndex, _paths.removeAt(oldIndex));
+      // });
+    }
+    setState(() => _draggedIndex = null);
+  }
+}
+
+extension on List<String> {
+  List<String> distinct() => {...this}.toList();
 }
