9caf9415106e47037c3a05a7be3a03effda59a2b
James Leahy <defuncart@gmail.com>
2022-02-26 09:12:47 +0100

    Add dropFileServiceProvider

--- lib/state/state.dart
+++ lib/state/state.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:photo_album/design_system/themes/adaptive_platform.dart';
+import 'package:photo_album/services/drop_file_service.dart';
 import 'package:photo_album/services/hive/hive_manager.dart';
 import 'package:photo_album/services/settings_database/i_settings_database.dart';
 import 'package:photo_album/services/settings_database/settings_database.dart';
@@ -10,6 +11,10 @@ final settingsDatabaseProvider = Provider<ISettingsDatabase>(
 
 final hiveManagerProvider = Provider(
   (_) => HiveManager(),
+);
+
+final dropFileServiceProvider = Provider(
+  (_) => DropFileService(),
 );
 
 final themeModeProvider = StateProvider((ref) => ref.watch(settingsDatabaseProvider).themeMode);
